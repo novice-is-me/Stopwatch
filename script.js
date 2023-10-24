45a490a431cc1438762d7da9c4f7f24b12d28a53
R.J <82492784+novice-is-me@users.noreply.github.com>
2023-10-24 23:14:05 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+
+function start(){
+
+}
+
+function pause(){
+
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+element.addEventListener('click', start());
+element.addEventListener('click', pause());
+element.addEventListener('click', stop());
+element.addEventListener('click', reset()); 
